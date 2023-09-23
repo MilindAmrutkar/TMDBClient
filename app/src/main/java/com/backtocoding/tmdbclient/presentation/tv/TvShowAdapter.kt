3f9f1a8f625d057948ee49558468d82bd6c22b11
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-09-23 08:45:57 +0530

    Added: binding of items to TvShow recyclerview

---
+++ app/src/main/java/com/backtocoding/tmdbclient/presentation/tv/TvShowAdapter.kt
@@ -0,0 +1,49 @@
+package com.backtocoding.tmdbclient.presentation.tv
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.backtocoding.tmdbclient.R
+import com.backtocoding.tmdbclient.data.model.tv.TvShow
+import com.backtocoding.tmdbclient.databinding.ListItemBinding
+import com.bumptech.glide.Glide
+
+class TvShowAdapter() : RecyclerView.Adapter<MyViewHolder>() {
+    private val tvShowList = ArrayList<TvShow>()
+
+    fun setList(tvShows: List<TvShow>) {
+        tvShowList.clear()
+        tvShowList.addAll(tvShows)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return tvShowList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(tvShowList[position])
+    }
+}
+
+class MyViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(tvShow: TvShow) {
+        binding.titleTextView.text = tvShow.name
+        binding.descriptionTextView.text = tvShow.overview
+        val posterUrl = "https://image.tmdb.org/t/p/w500" + tvShow.posterPath
+        Glide.with(binding.imageView.context)
+            .load(posterUrl)
+            .into(binding.imageView)
+    }
+}
